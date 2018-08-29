e339611a1f0d0f43ff39620b341fb886e6e1956a
1337_snax0r <40248826+mcoirad-gmmb@users.noreply.github.com>
2018-08-29 11:49:25 -0400

    try cat

--- R/read.R
+++ R/read.R
@@ -142,8 +142,8 @@ read_input <- function(x){
       out
     }
   } else if(is.character(x) && length(x) == 1 && !grepl("\n", x) && !is_pubkey_str(x)){
-    print(paste("Did find backslash ns?", grepl("\n", x)))
-    print(paste("In this file:", x))
+    cat(paste("Did find backslash ns?", grepl("\n", x), "\n"))
+    cat(paste("In this file:", x, "\n"))
     x <- normalizePath(path.expand(x))
     info <- file.info(x)
     stopifnot(!info$isdir)
